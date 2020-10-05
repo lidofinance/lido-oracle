e7b4d717ea6694402fbdcaa42116f6e10a8faf90
klyaus <d.suldin@ongrid.pro>
2020-10-05 16:57:48 +0300

    New logic
    Add StakingProvidersRegistry contract abi.
    Update logic for obtaining validators keys.
    Balances are always taken for the specified epoch or slot.

--- contracts.py
+++ contracts.py
@@ -1,10 +1,11 @@
-def get_validators_keys(contract, provider):
-    validators_keys_count = contract.functions.getTotalSigningKeyCount().call(
+def get_validators_keys(spr_id, contract, provider):
+    validators_keys_count = contract.functions.getTotalSigningKeyCount(spr_id).call(
         {'from': provider.eth.defaultAccount.address})
     validators_keys_list = []
     if validators_keys_count > 0:
         for index in range(validators_keys_count):
-            validator_key = contract.functions.getSigningKey(index).call({'from': provider.eth.defaultAccount.address})
+            validator_key = contract.functions.getSigningKey(spr_id, index).call(
+                {'from': provider.eth.defaultAccount.address})
             validators_keys_list.append(validator_key[0])
             index += 1
     return validators_keys_list
