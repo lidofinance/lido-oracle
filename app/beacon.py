a97081cbf0eefeb79b1e55d22b5bb05afa08a6ec
klyaus <d.suldin@ongrid.pro>
2020-10-15 17:35:46 +0300

    Lighthouse api breaking changes
    Change get last finalized and last actual slots method
    on issue https://github.com/depools/py-oracle/issues/22
    
    WIP

--- app/beacon.py
+++ app/beacon.py
@@ -5,13 +5,14 @@ import json
 import requests
 from requests.compat import urljoin
 
-api_version_lighthouse = 'node/version'
+api_version_lighthouse = 'eth/v1/node/version'
 api_version_prism = 'eth/v1alpha1/node/version'
 
 API = {
     'Lighthouse': {
-        'api_version': 'node/version',
-        'beacon_head': 'beacon/head',
+        'api_version': 'eth/v1/node/version',
+        'beacon_head_finalized': 'eth/v1/beacon/headers/finalized',
+        'beacon_head_actual': 'eth/v1/beacon/headers/head',
         'get_balances': 'beacon/validators',
         'get_slot': 'beacon/state_root'
     },
@@ -36,16 +37,19 @@ def get_beacon(provider):
 def get_actual_slots(beacon, provider):
     actual_slots = {}
     if beacon == "Lighthouse":
-        beacon_head = API['Lighthouse']['beacon_head']
-        response = requests.get(urljoin(provider, beacon_head)).json()
-        actual_slots['actual_slot'] = response['slot']
-        actual_slots['finalized_slot'] = response['finalized_slot']
+        beacon_head_actual = API['Lighthouse']['beacon_head_actual']
+        response = requests.get(urljoin(provider, beacon_head_actual)).json()
+        actual_slots['actual_slot'] = int(response['data']['header']['message']['slot'])
+
+        beacon_head_finalized = API['Lighthouse']['beacon_head_finalized']
+        response = requests.get(urljoin(provider, beacon_head_finalized)).json()
+        actual_slots['finalized_slot'] = int(response['data']['header']['message']['slot'])
         return actual_slots
     if beacon == "Prysm":
         beacon_head = API['Prysm']['beacon_head']
         response = requests.get(urljoin(provider, beacon_head)).json()
-        actual_slots['actual_slot'] = response['headSlot']
-        actual_slots['finalized_slot'] = response['finalizedSlot']
+        actual_slots['actual_slot'] = int(response['headSlot'])
+        actual_slots['finalized_slot'] = int(response['finalizedSlot'])
         return actual_slots
 
 
