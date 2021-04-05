56cebec99649fce14468b244a4b28a3b46316a57
Alexandr Tarelkin <tarelkinaa@bk.ru>
2021-04-05 18:14:33 +0300

    handling beacon node connection error

--- app/beacon.py
+++ app/beacon.py
@@ -22,6 +22,17 @@ def get_beacon(provider, slots_per_epoch):
         return Prysm(provider, slots_per_epoch)
     raise ValueError('Unknown beacon')
 
+class HandledException(Exception):
+    pass
+
+def error_handler(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            raise Exception(f'Handled exception: {type(exc).__name__}: {str(exc)}') from exc
+    return inner
+
 
 class Lighthouse:
     api_version = 'eth/v1/node/version'
@@ -37,12 +48,16 @@ class Lighthouse:
         self.slots_per_epoch = slots_per_epoch
         self.version = requests.get(urljoin(url, self.api_version)).json()
 
+    
+    @error_handler
     def get_finalized_epoch(self):
         return int(requests.get(urljoin(self.url, self.api_beacon_head_finality_checkpoints)).json()['data']['finalized']['epoch'])
 
+    @error_handler
     def get_genesis(self):
         return int(requests.get(urljoin(self.url, self.api_genesis)).json()['data']['genesis_time'])
 
+    @error_handler
     def get_actual_slot(self):
         actual_slots = {}
         response = requests.get(urljoin(self.url, self.api_beacon_head_actual)).json()
@@ -51,6 +66,7 @@ class Lighthouse:
         actual_slots['finalized_slot'] = int(response['data']['header']['message']['slot'])
         return actual_slots
 
+    @error_handler
     def _convert_key_list_to_str_arr(self, key_list):
         pubkeys = []
         for key in key_list:
@@ -58,6 +74,7 @@ class Lighthouse:
 
         return pubkeys
 
+    @error_handler
     def get_balances(self, slot, key_list):
         pubkeys = self._convert_key_list_to_str_arr(key_list)
 
@@ -104,16 +121,19 @@ class Prysm:
         self.slots_per_epoch = slots_per_epoch
         self.version = requests.get(urljoin(url, self.api_version)).json()
 
+    @error_handler
     def get_finalized_epoch(self):
         finalized_epoch = int(requests.get(urljoin(self.url, self.api_beacon_head)).json()['finalizedEpoch'])
         return finalized_epoch
 
+    @error_handler
     def get_genesis(self):
         genesis_time = requests.get(urljoin(self.url, self.api_genesis)).json()['genesisTime']
         genesis_time = datetime.datetime.strptime(genesis_time, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
         genesis_time = int(genesis_time.timestamp())
         return genesis_time
 
+    @error_handler
     def get_actual_slot(self):
         actual_slots = {}
         response = requests.get(urljoin(self.url, self.api_beacon_head)).json()
@@ -121,6 +141,7 @@ class Prysm:
         actual_slots['finalized_slot'] = int(response['finalizedSlot'])
         return actual_slots
 
+    @error_handler
     def get_balances(self, slot, key_list):
         params = {}
         pubkeys = []
