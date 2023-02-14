9931c29f94a068ae1ad0a49da523b1e6f25046b7
skhomuti <skhomuti@gmail.com>
2023-02-14 11:01:13 +0500

    Added reading env vars from .env file if present

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+if os.path.exists(".env"):
+    with open(".env", "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+            key, value = line.split("=")
+            os.environ[key] = value
