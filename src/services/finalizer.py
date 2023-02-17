78f5dce555c024b15384f2d583793d7a357d9530
rkolpakov <rkolpakov94@gmail.com>
2023-02-17 16:42:45 +0400

    feat: add basic tests for finalizer

--- src/services/finalizer.py
+++ src/services/finalizer.py
@@ -10,43 +10,43 @@ class Finalizer:
         self.w3 = w3
         self.safe_border_service = SafeBorder(w3)
 
-    def get_withdrawable_requests(self, is_bunker: bool, share_rate: int, blockstamp: BlockStamp) -> None | list(int):
-        (start_id, end_id) = self.get_unfinalized_withdrawal_ids(blockstamp)
+    def get_withdrawable_requests(self, is_bunker_mode: bool, share_rate: int, blockstamp: BlockStamp):
+        (first_request_id, last_request_id) = self._get_unfinalized_withdrawal_ids(blockstamp)
 
-        if end_id - start_id == 0:
+        if last_request_id - first_request_id == 0:
             return None
 
-        withdrawable_until_epoch = self.safe_border_service(is_bunker, blockstamp)
+        withdrawable_until_epoch = self.safe_border_service.get_safe_border_epoch(is_bunker_mode, blockstamp)
         withdrawable_until_timestamp = withdrawable_until_epoch * SLOTS_PER_EPOCH * SLOT_TIME
 
-        available_eth = self.fetch_locked_ether_amount(blockstamp)
+        available_eth = self._fetch_locked_ether_amount(blockstamp)
 
-        last_finalizable_request_id_by_timestamp = self.fetch_last_finalizable_request_id(withdrawable_until_timestamp, start_id, end_id, blockstamp)
-        last_finalizable_request_id_by_budget = self.fetch_last_finalizable_request_id(available_eth, share_rate, start_id, end_id, blockstamp)
-        end_id = min(last_finalizable_request_id_by_timestamp, last_finalizable_request_id_by_budget)
+        last_finalizable_request_id_by_timestamp = self._fetch_last_finalizable_request_id_by_timestamp(withdrawable_until_timestamp, first_request_id, last_request_id, blockstamp)
+        last_finalizable_request_id_by_budget = self._fetch_last_finalizable_request_id_by_budget(available_eth, share_rate, first_request_id, last_request_id, blockstamp)
+        last_request_id = min(last_finalizable_request_id_by_timestamp, last_finalizable_request_id_by_budget)
 
-        if end_id - start_id == 0:
+        if last_request_id - first_request_id <= 0:
             return None
         
-        return list(range(start_id, end_id + 1))
+        return list(range(first_request_id, last_request_id + 1))
 
-    def get_unfinalized_withdrawal_ids(self, blockstamp: BlockStamp):
-        start_id = self.fetch_last_finalized_request_id(blockstamp)
-        end_id = self.fetch_last_request_id(blockstamp)
+    def _get_unfinalized_withdrawal_ids(self, blockstamp: BlockStamp):
+        first_request_id = self._fetch_last_finalized_request_id(blockstamp)
+        last_request_id = self._fetch_last_request_id(blockstamp)
 
-        return (start_id, end_id)
+        return (first_request_id + 1, last_request_id)
 
-    def fetch_last_finalizable_request_id_by_budget(self, available_eth: int, share_rate: int, start_id: int, end_id: int, blockstamp: BlockStamp) -> int:
-        return self.w3.lido_contracts.withdrawal_queue.functions.findLastFinalizableRequestIdByBudget(available_eth, share_rate, start_id, end_id).call(block_identifier=blockstamp.block_hash)
+    def _fetch_last_finalizable_request_id_by_budget(self, available_eth: int, share_rate: int, first_request_id: int, last_request_id: int, blockstamp: BlockStamp) -> int:
+        return self.w3.lido_contracts.withdrawal_queue.functions.findLastFinalizableRequestIdByBudget(available_eth, share_rate, first_request_id, last_request_id).call(block_identifier=blockstamp.block_hash)
 
-    def fetch_last_finalizable_request_id_by_timestamp(self, max_timestamp: int, start_id: int, end_id: int, blockstamp: BlockStamp) -> int:
-        return self.w3.lido_contracts.withdrawal_queue.functions.getLastFinalizedRequestId(max_timestamp, start_id, end_id).call(block_identifier=blockstamp.block_hash)
+    def _fetch_last_finalizable_request_id_by_timestamp(self, max_timestamp: int, first_request_id: int, last_request_id: int, blockstamp: BlockStamp) -> int:
+        return self.w3.lido_contracts.withdrawal_queue.functions.getLastFinalizedRequestId(max_timestamp, first_request_id, last_request_id).call(block_identifier=blockstamp.block_hash)
 
-    def fetch_last_finalized_request_id(self, blockstamp: BlockStamp) -> int:
+    def _fetch_last_finalized_request_id(self, blockstamp: BlockStamp) -> int:
         return self.w3.lido_contracts.withdrawal_queue.functions.getLastFinalizedRequestId().call(block_identifier=blockstamp.block_hash)
 
-    def fetch_last_request_id(self, blockstamp: BlockStamp) -> int:
+    def _fetch_last_request_id(self, blockstamp: BlockStamp) -> int:
         return self.w3.lido_contracts.withdrawal_queue.functions.getLastRequestId().call(block_identifier=blockstamp.block_hash)
 
-    def fetch_locked_ether_amount(self, blockstamp: BlockStamp) -> int:
+    def _fetch_locked_ether_amount(self, blockstamp: BlockStamp) -> int:
         return self.w3.lido_contracts.withdrawal_queue.functions.getLockedEtherAmount().call(block_identifier=blockstamp.block_hash)
