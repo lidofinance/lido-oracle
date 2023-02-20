f67f74932742ea01ab9086dc1c18240adf5c87b7
skhomuti <skhomuti@gmail.com>
2023-02-20 15:10:50 +0500

    Cache web3 requests

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 
 from prometheus_client import start_http_server
 from web3_multi_provider import MultiProvider
+from web3.middleware import simple_cache_middleware
 
 from src import variables
 from src.metrics.healthcheck_server import start_pulse_server
@@ -66,6 +67,7 @@ if __name__ == '__main__':
 
     logger.info({'msg': 'Add metrics middleware for ETH1 requests.'})
     web3.middleware_onion.add(metrics_collector)
+    web3.middleware_onion.add(simple_cache_middleware)
 
     logger.info({'msg': 'Check protocol version.'})
     wait_for_withdrawals(web3)
