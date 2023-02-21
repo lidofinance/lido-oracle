2f611d99b4d3c789613159070cba2fcf1d8f3a79
skhomuti <skhomuti@gmail.com>
2023-02-21 16:02:25 +0500

    Fix f-string-without-interpolation

--- src/modules/accounting/bunker.py
+++ src/modules/accounting/bunker.py
@@ -366,7 +366,7 @@ class BunkerService:
             raise Exception("More than one ETHDistributed event found")
 
         if not events:
-            logger.info({"msg": f"No ETHDistributed event found. Vault withdrawals: 0 Gwei."})
+            logger.info({"msg": "No ETHDistributed event found. Vault withdrawals: 0 Gwei."})
             return 0
 
         vault_withdrawals = self.w3.from_wei(events[0]['args']['withdrawalsWithdrawn'], 'gwei')
