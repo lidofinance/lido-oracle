3038b3372ab2d0382895baf59c8e8e90792b4a05
skhomuti <skhomuti@gmail.com>
2023-03-01 16:50:42 +0500

    Skipping e2e tests when another tests are selected By default assumed that only fast unit and integration tests running E2e tests should be launched separately

--- tests/e2e/test_accounting.py
+++ tests/e2e/test_accounting.py
@@ -9,5 +9,5 @@ def test_app(start_accounting, caplog):
     wait_for_message_appeared(caplog, "{'msg': 'Check if main data was submitted.', 'value': False}")
     wait_for_message_appeared(caplog, "{'msg': 'Check if contract could accept report.', 'value': True}")
     wait_for_message_appeared(caplog, "{'msg': 'Execute module.'}")
-    wait_for_message_appeared(caplog, "{'msg': 'Checking bunker mode'}")
+    wait_for_message_appeared(caplog, "{'msg': 'Checking bunker mode'}", timeout=1800)
     wait_for_message_appeared(caplog, "{'msg': 'Send report hash. Consensus version: [1]'}")
