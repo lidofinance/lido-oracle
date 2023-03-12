b19a8a8e90e5dab5e20850b349ac04adf5fe53b5
vgorkavenko <vladimir.g@lido.fi>
2023-03-12 18:58:28 +0500

    fix after rebase

--- tests/factory/blockstamp.py
+++ tests/factory/blockstamp.py
@@ -2,13 +2,12 @@ from eth_typing import BlockNumber, HexStr
 from web3.types import Timestamp
 
 from tests.factory.web3_factory import Web3Factory
-from src.typings import BlockStamp, BlockRoot, StateRoot, SlotNumber, BlockHash, ReferenceBlockStamp, EpochNumber
+from src.typings import BlockStamp, StateRoot, SlotNumber, BlockHash, ReferenceBlockStamp, EpochNumber
 
 
 class BlockStampFactory(Web3Factory):
     __model__ = BlockStamp
 
-    block_root: BlockRoot = BlockRoot(HexStr('0x8cae2ea12fb6b488225277929e8905b533e3b09491b15d9948949ced9119c6da'))
     state_root: StateRoot = StateRoot(HexStr('0x623801c28526c1923f14e1bb5258e40a194059c42e280ee61c7189bf2fdbe05e'))
     slot_number: SlotNumber = SlotNumber(113500)
     block_hash: BlockHash = BlockHash(HexStr('0x4372578a683ba1c85c259a42492efbe0de9a28b1ac050b5e61065499ab80b0ca'))
@@ -19,7 +18,6 @@ class BlockStampFactory(Web3Factory):
 class ReferenceBlockStampFactory(Web3Factory):
     __model__ = ReferenceBlockStamp
 
-    block_root: BlockRoot = BlockRoot(HexStr('0x8cae2ea12fb6b488225277929e8905b533e3b09491b15d9948949ced9119c6da'))
     state_root: StateRoot = StateRoot(HexStr('0x623801c28526c1923f14e1bb5258e40a194059c42e280ee61c7189bf2fdbe05e'))
     slot_number: SlotNumber = SlotNumber(113500)
     block_hash: BlockHash = BlockHash(HexStr('0x4372578a683ba1c85c259a42492efbe0de9a28b1ac050b5e61065499ab80b0ca'))
