b6d9171c9954a171f50eceae9aac7241f0313c70
F4ever <1590415904a@gmail.com>
2023-10-03 18:14:40 +0200

    fix: Unhandled exception in oracle transaction submission

--- src/web3py/extensions/tx_utils.py
+++ src/web3py/extensions/tx_utils.py
@@ -3,14 +3,16 @@ from typing import Optional
 
 from eth_account.signers.local import LocalAccount
 from web3 import Web3
+from hexbytes import HexBytes
 from web3.contract.contract import ContractFunction
-from web3.exceptions import ContractLogicError
+from web3.exceptions import ContractLogicError, TimeExhausted
 from web3.module import Module
 from web3.types import TxReceipt, Wei, TxParams, BlockData
 
 from src import variables, constants
 from src.metrics.prometheus.basic import TRANSACTIONS_COUNT, Status
 from src.utils.input import prompt
+
 
 logger = logging.getLogger(__name__)
 
@@ -118,9 +120,12 @@ class TransactionUtils(Module):
         tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
         logger.info({"msg": "Transaction sent.", "value": tx_hash.hex()})
 
-        tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return self._handle_sent_transaction(tx_hash)
 
-        if not tx_receipt:
+    def _handle_sent_transaction(self, transaction_hash: HexBytes) -> Optional[TxReceipt]:
+        try:
+            tx_receipt = self.w3.eth.wait_for_transaction_receipt(transaction_hash)
+        except TimeExhausted:
             TRANSACTIONS_COUNT.labels(status=Status.FAILURE).inc()
             logger.warning({"msg": "Transaction was not found in blockchain after 120 seconds."})
             return None
