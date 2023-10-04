d7adef66035977664238c64adcf6d4c1ae34549c
F4ever <1590415904a@gmail.com>
2023-10-04 17:52:34 +0200

    fix empty list exception

--- src/utils/dataclass.py
+++ src/utils/dataclass.py
@@ -1,7 +1,7 @@
 import functools
 from dataclasses import dataclass, fields, is_dataclass
 from types import GenericAlias
-from typing import Callable, Self, Sequence, TypeVar
+from typing import Callable, Self, Sequence, TypeVar, Iterable
 
 from src.utils.abi import named_tuple_to_dataclass
 
@@ -60,6 +60,12 @@ def list_of_dataclasses(
         @functools.wraps(func)
         def wrapper_decorator(*args, **kwargs):
             list_of_elements = func(*args, **kwargs)
+
+            if not isinstance(list_of_elements, list) and not isinstance(list_of_elements, tuple):
+                raise DecodeToDataclassException(f'Type {type(list_of_elements)} is not supported.')
+
+            if not list_of_elements:
+                return list_of_elements
 
             if isinstance(list_of_elements[0], dict):
                 return list(map(lambda x: _dataclass_factory(**x), list_of_elements))
