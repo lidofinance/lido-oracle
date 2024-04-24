e71ef05bec336a454f9a9cee2c95aa3bc7311691
madlabman <10616301+madlabman@users.noreply.github.com>
2024-04-24 10:36:28 +0200

    chore: add a note about pinning

--- src/providers/ipfs/types.py
+++ src/providers/ipfs/types.py
@@ -11,7 +11,7 @@ class NotFound(Exception):
 
 
 class IPFSProvider(ABC):
-    """Interface for all implementations of an IPFS provider"""
+    """Interface for all implementations of an [IPFS](https://docs.ipfs.tech) provider"""
 
     @abstractmethod
     def fetch(self, cid: CIDv0 | CIDv1) -> bytes: ...
@@ -20,4 +20,5 @@ class IPFSProvider(ABC):
     def upload(self, content: bytes, name: str | None = None) -> CIDv0 | CIDv1: ...
 
     @abstractmethod
-    def pin(self, cid: CIDv0 | CIDv1) -> None: ...
+    def pin(self, cid: CIDv0 | CIDv1) -> None:
+        """Pin the content, see https://docs.ipfs.tech/how-to/pin-files"""
