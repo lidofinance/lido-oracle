789068cc84ebb96e877e4c57990cb5348190e827
Vladimir Gorkavenko <32727352+vgorkavenko@users.noreply.github.com>
2024-05-06 16:20:44 +0400

    feat: handle thread exception in processing (#443)
    * feat: handle thread exception in processing
    
    * fix: remove unused things
    
    * fix: use `ValueError`, shutdown the executor
    
    * fix: linter
    
    * chore: add a comment

--- src/modules/csm/checkpoint.py
+++ src/modules/csm/checkpoint.py
@@ -1,6 +1,7 @@
 import logging
 import time
-from threading import Thread, Lock
+from threading import Lock
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Any, Iterable, cast
 
 from src.modules.csm.state import State
@@ -84,13 +85,9 @@ class CheckpointsFactory:
 
 
 class Checkpoint:
-    # TODO: should be configurable or calculated based on the system resources
-    MAX_THREADS: int = 4
-
     cc: ConsensusClient
     converter: Web3Converter
 
-    threads: list[Thread]
     state: State
 
     slot: SlotNumber  # last slot of the epoch
@@ -110,37 +107,37 @@ class Checkpoint:
         self.slot = slot
         self.duty_epochs = duty_epochs
         self.block_roots = []
-        self.threads = []
         self.state = state
 
-    @property
-    def free_threads(self):
-        return self.MAX_THREADS - len(self.threads)
-
     def process(self, last_finalized_blockstamp: BlockStamp):
-        for duty_epoch in self.duty_epochs:
-            if duty_epoch in self.state.processed_epochs:
-                continue
-            if not self.block_roots:
-                self._get_block_roots()
-            roots_to_check = self._select_roots_to_check(duty_epoch)
-            if not self.free_threads:
-                self._await_oldest_thread()
-            # TODO: handle error in the thread. wait all, then raise
-            thread = Thread(
-                target=self._process_epoch, args=(last_finalized_blockstamp, duty_epoch, roots_to_check)
-            )
-            thread.start()
-            self.threads.append(thread)
-        self._await_all_threads()
+        def _unprocessed():
+            for _epoch in self.duty_epochs:
+                if _epoch in self.state.processed_epochs:
+                    continue
+                if not self.block_roots:
+                    self._get_block_roots()
+                yield _epoch
 
-    def _await_oldest_thread(self):
-        old = self.threads.pop(0)
-        old.join()
-
-    def _await_all_threads(self):
-        for thread in self.threads:
-            thread.join()
+        with ThreadPoolExecutor() as ext:
+            futures = {
+                ext.submit(self._process_epoch, last_finalized_blockstamp, duty_epoch): duty_epoch
+                for duty_epoch in _unprocessed()
+            }
+            for future in as_completed(futures):
+                duty_epoch = futures[future]
+                try:
+                    future.result()
+                except Exception as e:
+                    logger.error({
+                        "msg": f"Error processing epoch {duty_epoch} in thread, " +
+                               "wait the current threads and shutdown the executor",
+                        "error": str(e)
+                    })
+                    # Wait only for the current running threads to prevent
+                    # a lot of similar error-possible requests to the consensus node.
+                    # Raise the error after a batch of running threads is finished
+                    ext.shutdown(wait=True, cancel_futures=True)
+                    raise ValueError(e) from e
 
     def _select_roots_to_check(
         self, duty_epoch: EpochNumber
@@ -171,12 +168,11 @@ class Checkpoint:
         self,
         last_finalized_blockstamp: BlockStamp,
         duty_epoch: EpochNumber,
-        roots_to_check: list[BlockRoot]
     ):
         logger.info({"msg": f"Process epoch {duty_epoch}"})
         start = time.time()
         committees = self._prepare_committees(last_finalized_blockstamp, EpochNumber(duty_epoch))
-        for root in roots_to_check:
+        for root in self._select_roots_to_check(duty_epoch):
             if root is None:
                 continue
             slot_data = self.cc.get_block_details_raw(BlockRoot(root))
