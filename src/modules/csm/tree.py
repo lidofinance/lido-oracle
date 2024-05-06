42b7a8c61a9fc3381f8243c7946d1b7b6a462b90
madlabman <10616301+madlabman@users.noreply.github.com>
2024-05-06 11:05:17 +0300

    chore: drop YAML support TODO

--- src/modules/csm/tree.py
+++ src/modules/csm/tree.py
@@ -26,7 +26,7 @@ class Tree:
 
         try:
             return cls(StandardMerkleTree.load(json.loads(content)))
-        except json.JSONDecodeError as e:  # TODO: yaml is a way better, but no support out of the box.
+        except json.JSONDecodeError as e:
             raise ValueError("Unsupported tree format") from e
 
     def encode(self) -> bytes:
