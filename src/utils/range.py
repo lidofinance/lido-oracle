c21945662549e41405139ccedc7c54fb2ee76c06
madlabman <10616301+madlabman@users.noreply.github.com>
2024-06-11 10:22:34 +0200

    chore: typings for sequence function

--- src/utils/range.py
+++ src/utils/range.py
@@ -1,4 +1,10 @@
-def sequence(start: int, stop: int):
+from typing import cast, Iterable, TypeVar
+
+
+T = TypeVar("T", bound=int)
+
+
+def sequence(start: T, stop: T) -> Iterable[T]:
     """Returns inclusive range object [start;stop]"""
     assert stop > 0
-    return range(start, stop + 1)
+    return cast(Iterable, range(start, stop + 1))
