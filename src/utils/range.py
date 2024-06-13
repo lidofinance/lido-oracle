62c901a4404530cd007b18e1ba15f9a3bba4e2d3
madlabman <10616301+madlabman@users.noreply.github.com>
2024-06-13 12:00:10 +0200

    fix: State network average calculation

--- src/utils/range.py
+++ src/utils/range.py
@@ -1,4 +1,9 @@
-def sequence(start: int, stop: int):
+from typing import Iterable, TypeVar, cast
+
+T = TypeVar("T", bound=int)
+
+
+def sequence(start: T, stop: T) -> Iterable[T]:
     """Returns inclusive range object [start;stop]"""
     assert stop > 0
-    return range(start, stop + 1)
+    return cast(Iterable, range(start, stop + 1))
