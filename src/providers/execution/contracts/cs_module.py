b4368c9e30fb6f894954dee45f51f752aff46cba
madlabman <10616301+madlabman@users.noreply.github.com>
2024-06-14 15:18:39 +0200

    refactor: extract get_operators_with_stucks_in_range

--- src/providers/execution/contracts/cs_module.py
+++ src/providers/execution/contracts/cs_module.py
@@ -1,13 +1,10 @@
 import logging
-from itertools import groupby
-from typing import Callable, Iterable, NamedTuple, cast
+from typing import Iterable, NamedTuple
 
-from eth_typing import BlockNumber
-from web3.contract.contract import ContractEvent
-from web3.types import BlockIdentifier, EventData
+from web3.types import BlockIdentifier
+
 
 from src.utils.cache import global_lru_cache as lru_cache
-from src.utils.events import get_events_in_range
 from src.web3py.extensions.lido_validators import NodeOperatorId
 
 from ..base_interface import ContractInterface
@@ -43,27 +40,6 @@ class CSModuleContract(ContractInterface):
         for no_id in self.node_operators_ids(block):
             if self.node_operator_summary(no_id, block).stuckValidatorsCount > 0:
                 yield no_id
-
-    def new_stuck_operators_ids(self, l_block: BlockIdentifier, r_block: BlockIdentifier) -> Iterable[NodeOperatorId]:
-        """Returns node operators assumed to be stuck for the given frame (defined by the block identifiers)"""
-
-        l_block_number = self.w3.eth.get_block(l_block).get("number", BlockNumber(0))
-        r_block_number = self.w3.eth.get_block(r_block).get("number", BlockNumber(0))
-
-        by_no_id: Callable[[EventData], int] = lambda e: e["args"]["nodeOperatorId"]
-
-        events = sorted(
-            get_events_in_range(
-                cast(ContractEvent, self.events.StuckSigningKeysCountChanged),
-                l_block_number,
-                r_block_number,
-            ),
-            key=by_no_id,
-        )
-
-        for no_id, group in groupby(events, key=by_no_id):
-            if any(e["args"]["stuckKeysCount"] > 0 for e in group):
-                yield NodeOperatorId(no_id)
 
     def is_paused(self, block: BlockIdentifier = "latest") -> bool:
         resp = self.functions.isPaused().call(block_identifier=block)
--- src/web3py/extensions/csm.py
+++ src/web3py/extensions/csm.py
@@ -1,14 +1,17 @@
 import logging
 from functools import partial
+from itertools import groupby
 from time import sleep
-from typing import Iterable, cast
+from typing import Callable, Iterable, cast
 
+from eth_typing import BlockNumber
 from hexbytes import HexBytes
 from lazy_object_proxy import Proxy
 from web3 import Web3
+from web3.contract.contract import ContractEvent
 from web3.exceptions import Web3Exception
 from web3.module import Module
-from web3.types import BlockIdentifier
+from web3.types import BlockIdentifier, EventData
 
 from src import variables
 from src.metrics.prometheus.business import FRAME_PREV_REPORT_REF_SLOT
@@ -17,6 +20,7 @@ from src.providers.execution.contracts.cs_fee_oracle import CSFeeOracleContract
 from src.providers.execution.contracts.cs_module import CSModuleContract
 from src.providers.ipfs import CIDv0, CIDv1, is_cid_v0
 from src.types import BlockStamp, SlotNumber
+from src.utils.events import get_events_in_range
 from src.web3py.extensions.lido_validators import NodeOperatorId
 
 logger = logging.getLogger(__name__)
@@ -53,13 +57,38 @@ class CSM(Module):
         stuck: set[NodeOperatorId] = set()
         stuck.update(self.module.get_stuck_operators_ids(l_block))
         stuck.update(
-            self.module.new_stuck_operators_ids(
+            self.get_operators_with_stucks_in_range(
                 l_block,
                 r_block,
             )
         )
 
         return stuck
+
+    def get_operators_with_stucks_in_range(
+        self,
+        l_block: BlockIdentifier,
+        r_block: BlockIdentifier,
+    ) -> Iterable[NodeOperatorId]:
+        """Returns node operators assumed to be stuck for the given frame (defined by the block identifiers)"""
+
+        l_block_number = self.w3.eth.get_block(l_block).get("number", BlockNumber(0))
+        r_block_number = self.w3.eth.get_block(r_block).get("number", BlockNumber(0))
+
+        by_no_id: Callable[[EventData], int] = lambda e: e["args"]["nodeOperatorId"]
+
+        events = sorted(
+            get_events_in_range(
+                cast(ContractEvent, self.module.events.StuckSigningKeysCountChanged),
+                l_block_number,
+                r_block_number,
+            ),
+            key=by_no_id,
+        )
+
+        for no_id, group in groupby(events, key=by_no_id):
+            if any(e["args"]["stuckKeysCount"] > 0 for e in group):
+                yield NodeOperatorId(no_id)
 
     def _load_contracts(self) -> None:
         try:
