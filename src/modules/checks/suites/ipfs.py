fcb390bf5b8ece3094f419aabbb7c7a830d4be7c
madlabman <10616301+madlabman@users.noreply.github.com>
2024-06-20 16:14:04 +0200

    feat: IPFS checker all providers

--- src/modules/checks/suites/ipfs.py
+++ src/modules/checks/suites/ipfs.py
@@ -2,15 +2,29 @@
 
 
 import pytest
+from faker import Faker
+
+from src.main import ipfs_providers
+from src.providers.ipfs.public import PublicIPFS
+from src.providers.ipfs.types import IPFSError, IPFSProvider
 
 
 @pytest.fixture()
 def content():
-    return "ipfs.check"
+    return Faker().text()
 
 
-def check_ipfs_provider(web3, content: str):
+@pytest.mark.parametrize("provider", ipfs_providers(), ids=lambda p: p.__class__.__name__)
+def check_ipfs_provider(provider: IPFSProvider, content: str):
     """Checks that configured IPFS providers can be used by CSM"""
-    cid = web3.ipfs.publish(content.encode())
-    buf = web3.ipfs.fetch(cid)
-    assert buf.decode() == content
+
+    if isinstance(provider, PublicIPFS):
+        pytest.skip("PublicIPFS doesn't support pinning")
+
+    try:
+        cid = provider.publish(content.encode())
+        ret = provider.fetch(cid).decode()
+        if ret != content:
+            raise IPFSError(f"Content mismatch, got={ret}, expected={content}")
+    except IPFSError as e:
+        raise AssertionError(f"Provider {provider.__class__.__name__} is not working") from e
