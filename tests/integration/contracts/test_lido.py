63860712965eddaaea1fc3e97b864f0c0cc70dd3
F4ever <raman.s@lido.fi>
2024-09-20 11:55:00 +0200

    feat: change test order

--- tests/integration/contracts/test_lido.py
+++ tests/integration/contracts/test_lido.py
@@ -9,6 +9,9 @@ def test_lido_contract_call(lido_contract, accounting_oracle_contract, burner_co
     check_contract(
         lido_contract,
         [
+            ('get_buffered_ether', None, lambda response: check_value_type(response, int)),
+            ('total_supply', None, lambda response: check_value_type(response, int)),
+            ('get_beacon_stat', None, lambda response: check_value_type(response, BeaconStat)),
             (
                 'handle_oracle_report',
                 (
@@ -26,9 +29,6 @@ def test_lido_contract_call(lido_contract, accounting_oracle_contract, burner_co
                 ),
                 lambda response: check_value_type(response, LidoReportRebase),
             ),
-            ('get_buffered_ether', None, lambda response: check_value_type(response, int)),
-            ('total_supply', None, lambda response: check_value_type(response, int)),
-            ('get_beacon_stat', None, lambda response: check_value_type(response, BeaconStat)),
         ],
         caplog,
     )
