e1e8414429e9581731585239c54e60eac10ca325
hweawer <alexandergusakovsky@gmail.com>
2025-01-13 13:21:50 +0100

    Increase consensus

--- src/modules/accounting/accounting.py
+++ src/modules/accounting/accounting.py
@@ -58,7 +58,7 @@ class Accounting(BaseModule, ConsensusModule):
         - Send extra data
             Contains stuck and exited validators count by each node operator.
     """
-    COMPATIBLE_ONCHAIN_VERSIONS = [(1, 1), (2, 2)]
+    COMPATIBLE_ONCHAIN_VERSIONS = [(1, 1), (2, 2), (2, 3)]
 
     def __init__(self, w3: Web3):
         self.report_contract: AccountingOracleContract = w3.lido_contracts.accounting_oracle
