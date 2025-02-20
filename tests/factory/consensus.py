6d09761096e7a6fff6a2fde0b187761d3356b285
hweawer <alexandergusakovsky@gmail.com>
2025-02-20 09:48:05 +0100

    Fix

--- tests/factory/consensus.py
+++ tests/factory/consensus.py
@@ -1,11 +1,10 @@
 from typing import Any
 
-from src.providers.consensus.types import BeaconStateView, Validator
-from tests.factory.web3_factory import Web3Factory
+from src.providers.consensus.types import Validator, BeaconStateView
+from tests.factory.web3_factory import Web3DataclassFactory
 
 
-class BeaconStateViewFactory(Web3Factory):
-    __model__ = BeaconStateView
+class BeaconStateViewFactory(Web3DataclassFactory[BeaconStateView]):
 
     @classmethod
     def build_with_validators(cls, validators: list[Validator], **kwargs: Any):
