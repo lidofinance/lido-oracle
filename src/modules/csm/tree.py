84b9354229d754f330c38ce843f29aed9129fc2a
madlabman <10616301+madlabman@users.noreply.github.com>
2025-02-21 11:17:41 +0100

    chore: fix tree types inhereted fields

--- src/modules/csm/tree.py
+++ src/modules/csm/tree.py
@@ -1,36 +1,52 @@
 import json
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from json import JSONDecodeError, JSONDecoder, JSONEncoder
-from typing import Any, Iterable, Self, Sequence
+from typing import Any, ClassVar, Iterable, Self, Sequence
 
 from hexbytes import HexBytes
 from oz_merkle_tree import Dump, StandardMerkleTree
 
 from src.modules.csm.types import RewardsTreeLeaf, StrikesList, StrikesTreeLeaf
-from src.providers.ipfs.cid import CID
 from src.utils.types import hex_str_to_bytes
 
 
 class TreeJSONEncoder(JSONEncoder):
     def default(self, o):
-        if isinstance(o, StrikesList):
-            return list(o)
         if isinstance(o, bytes):
             return f"0x{o.hex()}"
-        if isinstance(o, CID):
-            return str(o)
         return super().default(o)
 
 
-@dataclass
+class TreeJSONDecoder(JSONDecoder):
+    # NOTE: object_pairs_hook is set unconditionally upon object initialisation, so it's required to
+    # override the __init__ method.
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs, object_pairs_hook=self.__object_pairs_hook)
+
+    @staticmethod
+    def __object_pairs_hook(items: list[tuple[str, Any]]):
+        def try_convert_all_hex_str_to_bytes(obj: Any):
+            if isinstance(obj, dict):
+                return {k: try_convert_all_hex_str_to_bytes(v) for (k, v) in obj.items()}
+            if isinstance(obj, list):
+                return [try_convert_all_hex_str_to_bytes(item) for item in obj]
+            if isinstance(obj, str) and obj.startswith("0x"):
+                return hex_str_to_bytes(obj)
+            return obj
+
+        return {k: try_convert_all_hex_str_to_bytes(v) for k, v in items}
+
+
 class Tree[LeafType: Iterable](ABC):
     """A wrapper around StandardMerkleTree to cover use cases of the CSM oracle"""
 
+    encoder: ClassVar[type[JSONEncoder]] = TreeJSONEncoder
+    decoder: ClassVar[type[JSONDecoder]] = TreeJSONDecoder
+
     tree: StandardMerkleTree[LeafType]
 
-    encoder: type[JSONEncoder] = TreeJSONEncoder
-    decoder: type[JSONDecoder] = JSONDecoder
+    def __init__(self, tree: StandardMerkleTree[LeafType]) -> None:
+        self.tree = tree
 
     @property
     def root(self) -> HexBytes:
@@ -74,26 +90,15 @@ class RewardsTree(Tree[RewardsTreeLeaf]):
         return cls(StandardMerkleTree(values, ("uint256", "uint256")))
 
 
-class StrikeTreeJSONDecoder(JSONDecoder):
-    # NOTE: object_pairs_hook is set unconditionally upon object initialisation, so it's required to
-    # override the __init__ method.
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs, object_pairs_hook=self.__object_pairs_hook)
-
-    @staticmethod
-    def __object_pairs_hook(items: list[tuple[str, Any]]):
-        def try_convert_all_hex_str_to_bytes(obj: Any):
-            if isinstance(obj, list):
-                return [try_convert_all_hex_str_to_bytes(item) for item in obj]
-            if isinstance(obj, str) and obj.startswith("0x"):
-                return hex_str_to_bytes(obj)
-            return obj
-
-        return {key: try_convert_all_hex_str_to_bytes(value) for key, value in items}
+class StrikesTreeJSONEncoder(TreeJSONEncoder):
+    def default(self, o):
+        if isinstance(o, StrikesList):
+            return list(o)
+        return super().default(o)
 
 
 class StrikesTree(Tree[StrikesTreeLeaf]):
-    decoder = StrikeTreeJSONDecoder
+    encoder = StrikesTreeJSONEncoder
 
     @classmethod
     def new(cls, values) -> Self:
