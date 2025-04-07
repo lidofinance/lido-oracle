cb0c20b9ee3e03cdf21a1b44d4a5b8a6e9c25be8
madlabman <10616301+madlabman@users.noreply.github.com>
2025-04-07 19:39:25 +0200

    refactor: rework structure and add config

--- kubo/020-pin-cids.sh
+++ kubo/020-pin-cids.sh
@@ -1,13 +1,6 @@
 #!/bin/env sh
 
-# Mount this file into docker container:
-# docker run \
-#     -p 4001:4001 -p 4001:4001/udp -p 127.0.0.1:8080:8080 -p 127.0.0.1:5001:5001 \
-#     -v ./010-pin-cids.sh:/container-init.d/010-pin-cids.sh \
-#     -v ./ipfs:/data/ipfs \
-#     ipfs/kubo:latest
-
-set -e
+set -eu
 
 WGET_ARGS="--quiet --no-check-certificate"
 GATEWAY=${GATEWAY:-https://ipfs.io}
@@ -17,7 +10,7 @@ echo "Fetching list of CIDs to pin"
 LIST=$(mktemp)
 trap "rm $LIST" EXIT
 
-wget https://raw.githubusercontent.com/lidofinance/csm-rewards/refs/heads/artifact/cids -O$LIST $WGET_ARGS
+wget https://raw.githubusercontent.com/lidofinance/csm-rewards/refs/heads/$CHAIN/cids -O$LIST $WGET_ARGS
 
 echo "Bootstrap pinned CIDs"
 while IFS= read -r cid; do
@@ -25,9 +18,12 @@ while IFS= read -r cid; do
     ipfs pin add ${cid} && continue || echo "Fetching ${cid} from remote"
 
     FILE=$(mktemp)
+    trap "rm $FILE" EXIT
+
     wget $GATEWAY/ipfs/${cid} -O$FILE $WGET_ARGS
-    ipfs add $FILE && rm $FILE
-    ipfs pin add $cid
+
+    imported_cid=$(ipfs add -q $FILE)
+    [ $imported_cid != $cid ] && { echo "Error: Importing $cid, but got $imported_cid"; exit 1; }
   }
 done < $LIST
 
--- kubo/entrypoint.sh
+++ kubo/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
+set -e
+
 # NOTE: crond has to be invoked by root, otherwise it won't be able to run a job.
 # crontab files have to have uid=0, and it's impossible to do via chown with no
 # root privileges. So all the crond-related commands are in the entrypoint.
 
 INIT_DIR=/container-init.d
 CRONTABS=$INIT_DIR/crontabs
-BOOTSTRAP_SCRIPT=$INIT_DIR/010-pin-cids.sh
+BOOTSTRAP_SCRIPT=$INIT_DIR/020-pin-cids.sh
 
 if [ ! -e $CRONTABS ]; then mkdir -p $CRONTABS; fi
 if [ ! -e $BOOTSTRAP_SCRIPT ]; then echo "Make sure $BOOTSTRAP_SCRIPT is mounted" && exit 1; fi
