35e40909bbe435c44db53835df6eb7d56cd3af8f
hweawer <alexandergusakovsky@gmail.com>
2025-04-07 21:07:24 +0200

    refactor: Remove delayed validators from VEBO predicate

--- tests/modules/ejector/test_validator_exit_order_iterator.py
+++ tests/modules/ejector/test_validator_exit_order_iterator.py
@@ -49,31 +49,6 @@ def test_get_filter_non_exitable_validators(iterator):
     assert filt(LidoValidatorFactory.build(index="1"))
 
 
-def test_get_delayed_validators(iterator):
-    iterator.lvs.get_operators_with_last_exited_validator_indexes = Mock(
-        return_value={
-            (1, 1): 2,
-            (1, 2): 3,
-        }
-    )
-
-    iterator.lvs.get_recently_requested_validators_by_operator = Mock(
-        return_value={
-            (1, 1): [2],
-            (1, 2): [3],
-        }
-    )
-
-    iterator.w3.lido_validators.get_lido_validators_by_node_operators = Mock(
-        return_value={
-            (1, 1): [LidoValidatorFactory.build(index="1"), LidoValidatorFactory.build(index="2")],
-            (1, 2): [LidoValidatorFactory.build(index="3"), LidoValidatorFactory.build(index="4")],
-        }
-    )
-
-    assert iterator._get_delayed_validators() == {(1, 1): 1, (1, 2): 0}
-
-
 def test_calculate_validators_age(iterator, monkeypatch):
     monkeypatch.setattr('src.services.exit_order_iterator.get_validator_age', lambda x, _: 1)
     iterator.blockstamp = ReferenceBlockStampFactory.build()
@@ -152,8 +127,6 @@ def test_eject_validator(iterator):
         }
     )
 
-    iterator._get_delayed_validators = Mock(return_value={(1, 1): 1, (1, 2): -1, (2, 1): -1})
-
     iterator._prepare_data_structure()
     iterator._calculate_lido_stats()
 
@@ -161,7 +134,6 @@ def test_eject_validator(iterator):
     assert iterator.module_stats[2].predictable_validators == 2
     assert iterator.node_operators_stats[(1, 1)].predictable_validators == 3
     assert iterator.node_operators_stats[(1, 1)].predictable_effective_balance == 3 * 32 * 10**9
-    assert iterator.node_operators_stats[(1, 1)].delayed_validators == 1
     assert iterator.node_operators_stats[(1, 2)].soft_exit_to is not None
     assert iterator.node_operators_stats[(2, 1)].force_exit_to is not None
     assert iterator.exitable_validators[(2, 1)][0].index == 7
@@ -211,7 +183,6 @@ def test_no_predicate(iterator):
     node_operator_1 = NodeOperatorStatsFactory.build(
         predictable_validators=100,
         predictable_effective_balance=Gwei(2000 * 32 * 10**9),
-        delayed_validators=1,
         total_age=1000,
         force_exit_to=50,
         soft_exit_to=25,
@@ -224,12 +195,11 @@ def test_no_predicate(iterator):
 
     result = iterator._no_predicate(node_operator_1)
 
-    assert result == (1, -50, -75, -185, 0, -100, 10)
+    assert result == (50, -75, -185, 0, -100, 10)
 
     node_operator_2 = NodeOperatorStatsFactory.build(
         predictable_validators=2000,
         predictable_effective_balance=Gwei(100 * 32 * 10**9),
-        delayed_validators=0,
         total_age=1000,
         force_exit_to=50,
         soft_exit_to=25,
@@ -241,23 +211,23 @@ def test_no_predicate(iterator):
     )
 
     result = iterator._no_predicate(node_operator_2)
-    assert result == (0, -1950, -1975, -185, -1000, -2000, 20)
+    assert result == (1950, -1975, -185, -1000, -2000, 20)
 
     iterator.consensus_version = 3
     iterator.w3.cc.is_electra_activated = Mock(return_value=False)
 
     # Check works with old alg before pectra
     result = iterator._no_predicate(node_operator_2)
-    assert result == (0, -1950, -1975, -185, -1000, -2000, 20)
+    assert result == (1950, -1975, -185, -1000, -2000, 20)
 
     iterator.w3.cc.is_electra_activated = Mock(return_value=True)
 
     # Check after pectra
     result = iterator._no_predicate(node_operator_2)
-    assert result == (0, -1950, -1975, -185, 0, -2000, 20)
+    assert result == (1950, -1975, -185, 0, -2000, 20)
 
     result = iterator._no_predicate(node_operator_1)
-    assert result == (1, -50, -75, -185, -1000, -100, 10)
+    assert result == (50, -75, -185, -1000, -100, 10)
 
 
 @pytest.mark.unit
