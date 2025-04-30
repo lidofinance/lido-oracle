bdfd2ca08281f0cb268209a92579c088b37baf0b
Sergey White <sergey.i@lido.fi>
2025-04-30 13:47:51 +0300

    test(lido_locator_contract): fix content of testing

--- tests/integration/contracts/test_lido.py
+++ tests/integration/contracts/test_lido.py
@@ -12,23 +12,6 @@ def test_lido_contract_call(lido_contract, accounting_oracle_contract, burner_co
             ('get_buffered_ether', None, lambda response: check_value_type(response, int)),
             ('total_supply', None, lambda response: check_value_type(response, int)),
             ('get_beacon_stat', None, lambda response: check_value_type(response, BeaconStat)),
-            (
-                'handle_oracle_report',
-                (
-                    1744632011,  # timestamp
-                    86400,
-                    389746,
-                    9303747261167584000000000,
-                    1337759360109000000000,
-                    119464421677104745350,
-                    0,
-                    accounting_oracle_contract.address,
-                    20390705,
-                    # Call depends on contract state
-                    '0xfbd1037bb43913198d5c8415a1d059afde09bf0dc56e250f5ff2bd2410c16dd2',
-                ),
-                lambda response: check_value_type(response, LidoReportRebase),
-            ),
         ],
         caplog,
     )
--- tests/integration/contracts/test_lido_locator.py
+++ tests/integration/contracts/test_lido_locator.py
@@ -10,16 +10,46 @@ def test_lido_locator_contract(lido_locator_contract, caplog):
         lido_locator_contract,
         [
             (
+                'accounting',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
+                'accounting_oracle',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
+                'burner',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
+                'el_rewards_vault',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
                 'lido',
                 None,
                 lambda response: check_value_re(ADDRESS_REGREX, response)
                 and check_value_type(response, ChecksumAddress),
             ),
             (
-                'accounting_oracle',
+                'oracle_daemon_config',
                 None,
                 lambda response: check_value_re(ADDRESS_REGREX, response)
-                and check_value_type(response, ChecksumAddress),
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
+                'oracle_report_sanity_checker',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
             ),
             (
                 'staking_router',
@@ -34,37 +64,19 @@ def test_lido_locator_contract(lido_locator_contract, caplog):
                 and check_value_type(response, ChecksumAddress),
             ),
             (
+                'vault_hub',
+                None,
+                lambda response: check_value_re(ADDRESS_REGREX, response)
+                                 and check_value_type(response, ChecksumAddress),
+            ),
+            (
                 'withdrawal_queue',
                 None,
                 lambda response: check_value_re(ADDRESS_REGREX, response)
                 and check_value_type(response, ChecksumAddress),
             ),
             (
-                'oracle_report_sanity_checker',
-                None,
-                lambda response: check_value_re(ADDRESS_REGREX, response)
-                and check_value_type(response, ChecksumAddress),
-            ),
-            (
-                'oracle_daemon_config',
-                None,
-                lambda response: check_value_re(ADDRESS_REGREX, response)
-                and check_value_type(response, ChecksumAddress),
-            ),
-            (
-                'burner',
-                None,
-                lambda response: check_value_re(ADDRESS_REGREX, response)
-                and check_value_type(response, ChecksumAddress),
-            ),
-            (
                 'withdrawal_vault',
-                None,
-                lambda response: check_value_re(ADDRESS_REGREX, response)
-                and check_value_type(response, ChecksumAddress),
-            ),
-            (
-                'el_rewards_vault',
                 None,
                 lambda response: check_value_re(ADDRESS_REGREX, response)
                 and check_value_type(response, ChecksumAddress),
