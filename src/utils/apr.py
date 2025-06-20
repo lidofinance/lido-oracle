76480b4015137479d534e30e24f2a8ef2a576d0e
Sergey White <sergey.i@lido.fi>
2025-06-20 12:31:10 +0300

    tets(vaults): trying get apr

--- src/utils/apr.py
+++ src/utils/apr.py
@@ -4,6 +4,8 @@ from src.modules.accounting.types import SECONDS_IN_YEAR
 def predict_apr(pre_total_shares: int, pre_total_ether: int, post_total_shares: int, post_total_ether: int,
                  time_elapsed: int) -> int:
     """
+    https://github.com/lidofinance/lido-eth-api-backend/blob/develop/src/apr/steth/steth-apr.service.ts
+
     Compute user-facing APR using share rate growth over time.
     Formula follows Lido V2-style:
         apr = (postRate - preRate) * SECONDS_IN_YEAR / preRate / timeElapsed
@@ -19,9 +21,11 @@ def predict_apr(pre_total_shares: int, pre_total_ether: int, post_total_shares: 
     # if rate_diff == 0:
     #     raise ValueError("Cannot compute APR. rate_diff is 0")
 
+    mul_for_precision = 10_0000
+
     if time_elapsed == 0:
         raise ValueError("Cannot compute APR. time_elapsed is 0")
 
-    apr = (rate_diff * SECONDS_IN_YEAR * 100) // (pre_rate * time_elapsed)
+    apr = (rate_diff * mul_for_precision * SECONDS_IN_YEAR * 100) / (pre_rate * time_elapsed)
 
-    return apr // 10 ** 5
+    return apr / mul_for_precision
