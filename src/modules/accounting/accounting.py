ffc72d298aae7f99a8b2fc32a60fdccfdca3b550
Sergey White <sergey.i@lido.fi>
2025-06-23 17:43:42 +0300

    feat(vaults): fees review

--- src/modules/accounting/accounting.py
+++ src/modules/accounting/accounting.py
@@ -63,7 +63,7 @@ from src.web3py.types import Web3
 
 logger = logging.getLogger(__name__)
 
-TOTAL_BASIS_POINTS = 100_00
+TOTAL_BASIS_POINTS = 10_000
 
 class Accounting(BaseModule, ConsensusModule):
     """
@@ -492,7 +492,8 @@ class Accounting(BaseModule, ConsensusModule):
         modules_fee, treasury_fee, base_precision  = self.w3.lido_contracts.staking_router.get_staking_fee_aggregate_distribution(blockstamp.block_hash)
         lido_fee_bp = ((modules_fee + treasury_fee) / base_precision) * TOTAL_BASIS_POINTS
 
-        # TODO: check lido_fee_bp precision and fail if >= 100_00
+        if lido_fee_bp >= 10_000:
+            raise ValueError(f"Got incorrect lido_fee_bp: {lido_fee_bp} >= 10_000 bp")
 
         chain_conf = self.get_chain_config(blockstamp)
         slots_elapsed = self._get_slots_elapsed_from_last_report(blockstamp)
@@ -542,9 +543,9 @@ class Accounting(BaseModule, ConsensusModule):
             blocks_elapsed = current_block - prev_block_number
 
             liquidity_fee = vault_info.liquidity_feeBP
-            vault_infrastructure_fee = 0
-            vault_reservation_liquidity_fee = 0
-            vault_liquidity_fee = 0
+            vault_infrastructure_fee: float = 0
+            vault_reservation_liquidity_fee: float = 0
+            vault_liquidity_fee: float = 0
 
             # Infrastructure fee = Total_value * Lido_Core_APR * Infrastructure_fee_rate
             vault_infrastructure_fee += (
@@ -559,7 +560,7 @@ class Accounting(BaseModule, ConsensusModule):
                     vault_info.mintable_capacity_StETH
                     * blocks_elapsed
                     * core_apr_ratio
-                    * vault_info.reservation_feeBP // (BLOCKS_PER_YEAR * 100_00 * 100_00)
+                    * vault_info.reservation_feeBP // (BLOCKS_PER_YEAR * TOTAL_BASIS_POINTS)
             )
 
             # Liquidity fee = Minted_stETH * Lido_Core_APR * Liquidity_fee_rate
@@ -569,14 +570,14 @@ class Accounting(BaseModule, ConsensusModule):
             # In case of no events, we just use `liability_shares` to calculate minted stETH.
             if vault_address not in events:
                 minted_steth = self._get_ether_by_shares(liability_shares, simulation.pre_total_pooled_ether, simulation.pre_total_shares)
-                vault_liquidity_fee += (minted_steth * blocks_elapsed * core_apr * liquidity_fee) // (BLOCKS_PER_YEAR * 100_00 * 100_00)
+                vault_liquidity_fee += (minted_steth * blocks_elapsed * core_apr_ratio * liquidity_fee) // (BLOCKS_PER_YEAR * TOTAL_BASIS_POINTS)
 
             # In case of events, we iterate through them backwards, calculating liquidity fee for each interval based
             # on the `liability_shares` and the elapsed blocks between events.
             for event in events[vault_address]:
                 blocks_elapsed_event = current_block - event.block_number
                 minted_steth_event = self._get_ether_by_shares(liability_shares, simulation.pre_total_pooled_ether, simulation.pre_total_shares)
-                vault_liquidity_fee += (minted_steth_event * blocks_elapsed_event * core_apr * liquidity_fee) // (BLOCKS_PER_YEAR * 100_00 * 100_00)
+                vault_liquidity_fee += (minted_steth_event * blocks_elapsed_event * core_apr_ratio * liquidity_fee) // (BLOCKS_PER_YEAR * TOTAL_BASIS_POINTS)
 
                 if isinstance(event, VaultFeesUpdatedEvent):
                     liquidity_fee = event.prev_liquidity_fee_bp
@@ -598,6 +599,6 @@ class Accounting(BaseModule, ConsensusModule):
                     raise ValueError(f"Wrong liability shares by vault {vault_address}")
 
             vault_total_fees_for_period = vault_infrastructure_fee + vault_reservation_liquidity_fee + vault_liquidity_fee
-            out[vault_info.id()] = int(prev_fee[vault_address]) + vault_total_fees_for_period
+            out[vault_info.id()] = int(prev_fee[vault_address]) + int(vault_total_fees_for_period)
 
         return out
