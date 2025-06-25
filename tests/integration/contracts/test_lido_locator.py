2233a01003ad67057df6872343177ce3a1b53b1a
chasingrainbows <chasing.rainbows@lido.fi>
2025-06-25 14:17:11 +0300

    feat(vault-pectra): move part of test_lido_locator_contract to testnet

--- tests/integration/contracts/test_lido_locator.py
+++ tests/integration/contracts/test_lido_locator.py
@@ -4,6 +4,7 @@ from eth_typing import ChecksumAddress
 from tests.integration.contracts.contract_utils import check_contract, check_is_instance_of
 
 
+@pytest.mark.mainnet
 @pytest.mark.integration
 def test_lido_locator_contract(lido_locator_contract, caplog):
     check_contract(
@@ -11,8 +12,6 @@ def test_lido_locator_contract(lido_locator_contract, caplog):
         [
             ('lido', None, check_is_instance_of(ChecksumAddress)),
             ('accounting_oracle', None, check_is_instance_of(ChecksumAddress)),
-            ('accounting', None, check_is_instance_of(ChecksumAddress)),
-            ('vault_hub', None, check_is_instance_of(ChecksumAddress)),
             ('staking_router', None, check_is_instance_of(ChecksumAddress)),
             ('validator_exit_bus_oracle', None, check_is_instance_of(ChecksumAddress)),
             ('withdrawal_queue', None, check_is_instance_of(ChecksumAddress)),
@@ -21,6 +20,19 @@ def test_lido_locator_contract(lido_locator_contract, caplog):
             ('burner', None, check_is_instance_of(ChecksumAddress)),
             ('withdrawal_vault', None, check_is_instance_of(ChecksumAddress)),
             ('el_rewards_vault', None, check_is_instance_of(ChecksumAddress)),
+        ],
+        caplog,
+    )
+
+
+@pytest.mark.testnet
+@pytest.mark.integration
+def test_lido_locator_contract_testnet(lido_locator_contract, caplog):
+    check_contract(
+        lido_locator_contract,
+        [
+            ('accounting', None, check_is_instance_of(ChecksumAddress)),
+            ('vault_hub', None, check_is_instance_of(ChecksumAddress)),
         ],
         caplog,
     )
