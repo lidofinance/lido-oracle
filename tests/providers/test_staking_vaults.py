f9a0246fb1df09fbf28c1ef9a41a6c93db1c928e
Sergey White <sergey.i@lido.fi>
2025-07-10 19:56:39 +0300

    test(vaults): black formatter

--- tests/providers/test_staking_vaults.py
+++ tests/providers/test_staking_vaults.py
@@ -7,15 +7,21 @@ from eth_typing import BlockNumber
 class TestStakingVaultsContractsSmoke:
 
     def test_get_burned_events(self, web3_integration):
-        events = web3_integration.lido_contracts.vault_hub.get_burned_events(BlockNumber(634086 - 1_000), BlockNumber(634086))
+        events = web3_integration.lido_contracts.vault_hub.get_burned_events(
+            BlockNumber(634086 - 1_000), BlockNumber(634086)
+        )
         assert len(events) != 0
 
     def test_get_minted_events(self, web3_integration):
-        events = web3_integration.lido_contracts.vault_hub.get_minted_events(BlockNumber(634086 - 1_000), BlockNumber(634086))
+        events = web3_integration.lido_contracts.vault_hub.get_minted_events(
+            BlockNumber(634086 - 1_000), BlockNumber(634086)
+        )
         assert len(events) != 0
 
     def test_get_updated_events(self, web3_integration):
-        events = web3_integration.lido_contracts.vault_hub.get_vault_fee_updated_events(BlockNumber(634086 - 1_000), BlockNumber(634_086))
+        events = web3_integration.lido_contracts.vault_hub.get_vault_fee_updated_events(
+            BlockNumber(634086 - 1_000), BlockNumber(634_086)
+        )
         assert len(events) == 0
 
     def test_staking_fee_aggregate_distribution(self, web3_integration):
