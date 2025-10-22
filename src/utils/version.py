2ea57863cb2d055e20124f06a188c1211ec84731
chasingrainbows <chasing.rainbows@lido.fi>
2025-10-22 12:52:46 +0300

    feat(orc-503): add cache for get_oracle_version

--- src/utils/version.py
+++ src/utils/version.py
@@ -1,7 +1,9 @@
 import os
 import tomllib
+from functools import lru_cache
 
 
+@lru_cache(maxsize=1)
 def get_oracle_version() -> str:
     try:
         pyproject_path = os.path.join(os.path.dirname(__file__), "..", "..", "pyproject.toml")
