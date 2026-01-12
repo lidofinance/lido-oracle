593178ab61c2a5fd2ce4f7a509f4eb651518deb2
chasingrainbows <chasing.rainbows@lido.fi>
2026-01-12 20:16:41 +0300

    feat(orc-372): fix polyfactory after bump

--- tests/factory/web3_factory.py
+++ tests/factory/web3_factory.py
@@ -52,7 +52,6 @@ class Web3DataclassFactory(Generic[T], BaseFactory[T]):
                     annotation=model_type_hints[field.name],
                     name=field.name,
                     default=default_value,
-                    random=cls.__random__,
                 ),
             )
 
