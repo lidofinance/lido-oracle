05d4fa98a09bfd9b95d70e649f3e36d82bfe1209
hweawer <alexandergusakovsky@gmail.com>
2026-04-10 13:21:52 +0200

    fix: skip test curated staking module

--- tests/integration/contracts/test_curated_staking_module.py
+++ tests/integration/contracts/test_curated_staking_module.py
@@ -4,14 +4,15 @@ from tests.integration.contracts.contract_utils import check_contract, check_val
 
 
 @pytest.mark.mainnet
+@pytest.mark.skip("Enable after srv3 upgrade")
 @pytest.mark.integration
 def test_curated_staking_module_contract(curated_staking_module_contract, caplog):
     check_contract(
         curated_staking_module_contract,
         [
-            # ('get_operator_weights', ([0, 1, 2], 'latest'), lambda r: check_value_type(r, list)),
+            ('get_operator_weights', ([0, 1, 2], 'latest'), lambda r: check_value_type(r, list)),
             ('get_type', ('latest',), lambda r: check_value_type(r, bytes)),
-            # ('get_meta_registry_address', ('latest',), lambda r: check_value_type(r, str)),
+            ('get_meta_registry_address', ('latest',), lambda r: check_value_type(r, str)),
         ],
         caplog,
     )
